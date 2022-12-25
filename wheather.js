6c4d3c9e9721b341de3bb911afab26c05a93c62f
Ilya Tarasov <ilyatarasov75@gmail.com>
2022-12-26 00:50:26 +0300

    fix

--- wheather.js
+++ wheather.js
@@ -120,7 +120,7 @@ const wheatherFromHistory = function () {
         <div class="d-flex flex-column">
             <div class="d-flex flex-row">
                 <div class="d-flex align-items-center">
-                    <span id="temp_c">${content.current.temp_c}</span>
+                    <span id="temp_c">C<sup>0</sup> ${content.current.temp_c}</span>
                 </div>
                 <div>
                     <img id="wheather-icon" src="https:${content.current.condition.icon}" alt="wheather" />
@@ -130,16 +130,16 @@ const wheatherFromHistory = function () {
                         <span id="wheather-name">${content.current.condition.text}</span>
                     </div>
                     <div>
-                        <span id="feeling-temp">${content.current.feelslike_c}</span>
+                        <span id="feeling-temp">По ощущениям ${content.current.feelslike_c}</span>
                     </div>
                 </div>
             </div>
             <div class="d-flex flex-row justify-content-between mb-2">
                 <div>
-                    <span id="wing-speed">${content.current.wind_mph}</span>
+                    <span id="wing-speed">Ветер км/ч ${content.current.wind_mph}</span>
                 </div>
                 <div>
-                    <span id="humidity">${content.current.humidity}</span>
+                    <span id="humidity">Влажность ${content.current.humidity}</span>
                 </div>
                 <div>
                     <span></span>
